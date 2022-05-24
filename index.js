2f0c327578616a7e1f57456a8b8c7baa3b18fcf0
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-24 23:03:25 +0600

    get product by id

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ async function run() {
             res.send(products);
         });
 
+        // Get product by id
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        })
+
         // Get all reviews
         app.get("/reviews", async (req, res) => {
             const reviews = await reviewsCollection.find().toArray();
