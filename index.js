0dfeeeae68671b7d8455089c79363ee2656e27a1
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-24 16:19:08 +0600

    add user done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,6 +22,7 @@ async function run() {
         await client.connect();
         const productsCollection = client.db("keyBoardMania").collection("products");
         const reviewsCollection = client.db("keyBoardMania").collection("reviews");
+        const usersCollection = client.db("keyBoardMania").collection("users");
 
         // Get all products
         app.get("/products", async (req, res) => {
@@ -33,6 +35,20 @@ async function run() {
             const reviews = await reviewsCollection.find().toArray();
             res.send(reviews);
         });
+
+        // Add user in DB
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+        })
     } finally {
     }
 }
